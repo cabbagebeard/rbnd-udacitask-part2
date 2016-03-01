64d347ab3593c272303f8b6aa3f5d2cad04b1803
Kevin Windisch <kwindisch1992@gmail.com>
2016-03-01 03:15:19 +0000

    Refactored format_date

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,18 @@
 module Listable
-  # Listable methods go here
+  def format_description(description)
+    "#{description}".ljust(30)
+  end
+  def format_date(options ={})
+    if options[:due] then
+        dates = options[:due].strftime("%D")
+      elsif options[:start_date]
+        dates = options[:start_date].strftime("%D") 
+        if options[:end_date] then
+          dates << " -- " + options[:end_date].strftime("%D")
+        end
+      else
+        dates = "N/A"
+    end
+    return dates
+  end
 end
