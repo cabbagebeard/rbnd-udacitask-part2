e205e73a0a0db1d83d8fb3ecca3c1d79c705b4ae
Kevin Windisch <kwindisch1992@gmail.com>
2016-04-06 15:28:34 -0500

    formatting todo.rb

--- lib/todo.rb
+++ lib/todo.rb
@@ -8,12 +8,14 @@ class TodoItem
     @due = options[:due] ? Chronic.parse(options[:due]) : options[:due]
     @priority = options[:priority]
   end
+  
   def details
     "Todo: ".colorize(:green) +  format_description(@description) + "due: " + format_date(due: @due) + format_priority
   end
   def type_name
     "todo"
   end
+  
   def priority
     @priority
   end
